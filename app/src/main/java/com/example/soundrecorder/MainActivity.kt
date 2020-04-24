72f8090c6abcc23b09f81b8280e63665f30ae689
dev-juri <ojurifemi132@gmail.com>
2020-04-24 19:35:31 +0100

    Added Chronometer and dataBinding

--- app/src/main/java/com/example/soundrecorder/MainActivity.kt
+++ app/src/main/java/com/example/soundrecorder/MainActivity.kt
@@ -5,30 +5,26 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.core.app.ActivityCompat
-import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.playlist_btn
-import kotlinx.android.synthetic.main.activity_main.stop_icon_house
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.databinding.DataBindingUtil
+import com.example.soundrecorder.databinding.ActivityMainBinding
 import java.io.IOException
+import kotlin.random.Random
 
 private const val LOG_TAG = "AudioRecordTest"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class MainActivity : AppCompatActivity() {
 
-
     private var fileName: String = " "
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(android.Manifest.permission.RECORD_AUDIO)
-    private var recorder: MediaRecorder ?= null
+    private var recorder: MediaRecorder? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -38,14 +34,14 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
             grantResults[0] == PackageManager.PERMISSION_GRANTED
-        }else{
+        } else {
             false
         }
-        if(!permissionToRecordAccepted) finish()
+        if (!permissionToRecordAccepted) finish()
     }
 
-    private fun startRecording(){
-        recorder=
+    private fun startRecording() {
+        recorder =
             MediaRecorder().apply {
                 setAudioSource(MediaRecorder.AudioSource.MIC)
                 setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
@@ -54,14 +50,14 @@ class MainActivity : AppCompatActivity() {
 
                 try {
                     prepare()
-                }catch (e: IOException) {
+                } catch (e: IOException) {
                     Log.e(LOG_TAG, "prepare() failed")
                 }
                 start()
             }
     }
 
-    private fun stopRecording(){
+    private fun stopRecording() {
         recorder?.apply {
             stop()
             release()
@@ -70,53 +66,60 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        var file_name = findViewById<TextView>(R.id.filename)
-        var record_state = findViewById<TextView>(R.id.record_state_text)
-        var record_start = findViewById<ImageButton>(R.id.record_start)
-        var icon_house = findViewById<LinearLayout>(R.id.stop_icon_house)
-        var playlistbtn = findViewById<ImageButton>(R.id.playlist_btn)
+        val endRecordingStateText = "Tap the microphone to start recording"
+        val startRecording = "Recording..."
 
-        record_start.apply {
-            setOnClickListener(){
-                startRecording()
-                file_name.text = fileName
-                record_state.text = "Recording..."
-                icon_house.visibility = View.VISIBLE
-                isClickable = false
-                playlistbtn.isClickable = false
-
-            }
-
-        }
-        stop_recording.apply {
-            setOnClickListener(){
-                stopRecording()
-                icon_house.visibility = View.INVISIBLE
-                record_start.isClickable = true
-                playlistbtn.isClickable = true
-                record_state.text = "Tap the microphone to start recording"
-
-            }
-
+        binding.recordStart.setOnClickListener {
+            fileName = "${externalCacheDir?.absolutePath}/${randomName()}.3gp"
+            binding.recordCounter.base = SystemClock.elapsedRealtime()
+            startRecording()
+            binding.recordCounter.start()
+            binding.recordStateText.text = startRecording
+            toastAnAlert(startRecording)
+            binding.stopIconHouse.visibility = View.VISIBLE
+            binding.playlistBtn.isClickable = false
+            binding.playlistBtn.isFocusable = false
         }
 
-        playlist_btn.apply{
-            setOnClickListener(){
+        binding.stopRecording.setOnClickListener() {
+            stopRecording()
+            binding.recordCounter.stop()
+            binding.recordCounter.base = SystemClock.elapsedRealtime()
+            binding.stopIconHouse.visibility = View.GONE
+            toastAnAlert("New Recording: ${randomName()}.3gp saved")
+            binding.recordStart.isClickable = true
+            binding.recordStart.isFocusable = true
+            binding.playlistBtn.isFocusable = true
+            binding.playlistBtn.isClickable = true
+            binding.recordStateText.text = endRecordingStateText
+        }
+
+        binding.playlistBtn.setOnClickListener() {
             playlist()
-            }
         }
     }
 
-    private fun playlist(){
+    private fun playlist() {
         var intent = Intent(applicationContext, PlaylistActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun randomName(): String {
+        var txt_part: String = "Recording"
+        var randomInt: Int = Random.nextInt(1000)
+        fileName = "$txt_part$randomInt"
+        return fileName
+    }
+
+    private fun toastAnAlert(text: String) {
+        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+
+        return toast.show()
     }
 
 
--- app/src/main/java/com/example/soundrecorder/PlaylistActivity.kt
+++ app/src/main/java/com/example/soundrecorder/PlaylistActivity.kt
@@ -3,19 +3,16 @@ package com.example.soundrecorder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.soundrecorder.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_playlist.*
 
 class PlaylistActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_playlist)
-
-        recorder_activity.apply {
-            setOnClickListener(){
-                var intent = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intent)
-            }
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_playlist)
     }
 }
